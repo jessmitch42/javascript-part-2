2b493689a369bb097e0f64c800286eee2dd0a6ae
JessMitch <jessicamitch42@gmail.com>
2016-07-06 21:03:06 +0000

    done!

--- warriors.js
+++ warriors.js
@@ -17,3 +17,46 @@ which player won the fight based on their power. Be as creative as
 you like with the text of this method :)
 Finally, create a bunch of warriors and make them fight together.
 */
+
+function Warrior(name, gender) {
+    this.name = name;
+    if (gender === "M" || gender === "F") {
+        this.gender = gender 
+    };
+    this.level = 1;
+    this.weapon =  "wooden sword";
+    this.power = Math.floor(Math.random()*100+1)
+}
+
+Warrior.prototype = {
+    fight: function() {
+        console.log(this.name + " rushes in the arena with " + this.weapon);
+    },
+    faceoff: function(opponent) {
+        if (this.power > opponent.power) {
+        console.log("After an epic fight, " + opponent.name + " dies in a pool of blood");
+        }
+        else if (this.power < opponent.power) {
+        console.log("After an epic fight, " + this.name + " is dead, dead, dead! " + opponent.name + " wins!");
+        }
+        else {
+        console.log("It's a draw! We're all just standing around |_(ツ)_/¯");
+        }
+}
+};
+
+var jess = new Warrior("Jess", "F");
+var max = new Warrior("Maxime", "M");
+var dennis = new Warrior("Dennis", "M");
+var dylan = new Warrior("Dylan", "M");
+var kash = new Warrior("Kash", "F");
+var celine = new Warrior("Celine", "F");
+
+jess.fight();
+jess.faceoff(max);
+
+dennis.fight();
+dennis.faceoff(dylan);
+
+kash.fight();
+kash.faceoff(celine);
